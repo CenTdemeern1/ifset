3f6d8f1fa6b5c513a06ad598d5fb35b03fba50fe
CenTdemeern1 <timo.herngreen@gmail.com>
2022-09-30 20:25:35 +0200

    Update main.py with newfound Rust knowledge

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,6 @@ struct Position {
     indentation : usize,
 }
 
-struct InterpreterState {
-    memory: HashMap<String, String>,
-    functions: HashMap<String, Position>,
-    stack: Vec<Position>,
-    current_position: Position,
-    term: Term,
-}
-
 fn read_file() -> String {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
@@ -30,13 +22,84 @@ fn read_file() -> String {
     code
 }
 
-fn sanitize_value(value: String, state: &InterpreterState) -> String {
-    let mut value = value.clone();
-    while value.contains(r"\INPUT") {
-        let input = state.term.read_char().expect("Couldn't read input.").to_string();
-        value = value.replacen(r"\INPUT", &input, 1);
+struct InterpreterState {
+    memory: HashMap<String, String>,
+    functions: HashMap<String, Position>,
+    stack: Vec<Position>,
+    current_position: Position,
+    term: Term,
+}
+
+impl InterpreterState {
+    fn sanitize_value(&self, value: String) -> String {
+        let mut value = value.clone();
+        while value.contains(r"\INPUT") {
+            let input = self.term.read_char().expect("Couldn't read input.").to_string();
+            value = value.replacen(r"\INPUT", &input, 1);
+        }
+        value.replace(r"\n", "\n").replace(r"\r", "\r").replace(r"\\", "\\")
     }
-    value.replace(r"\n", "\n").replace(r"\r", "\r").replace(r"\\", "\\")
+
+    fn run_line(&mut self, line: &String) {
+        let stripped_line = line.trim_start_matches("\t");
+        let line_indentation = line.len() - stripped_line.len();
+        if line_indentation > self.current_position.indentation {
+            return;
+        }
+        if line_indentation < self.current_position.indentation {
+            self.current_position.indentation = line_indentation;
+        }
+        let line = stripped_line.to_string();
+        if let Some((key, value)) = match_assignment(&line) {
+            let value = self.sanitize_value(value);
+            if key == "OUTPUT" {
+                print!("{}", value);
+                return;
+            }
+            self.memory.insert(key, value);
+            return;
+        }
+        if let Some((key, value)) =  match_if(&line) {
+            let value = self.sanitize_value(value);
+            match self.memory.get(&key) {
+                Some(mem_value) => {
+                    if value == *mem_value {
+                        self.current_position.indentation += 1;
+                    }
+                }
+                None => panic!("Variable {:?} is not defined", key),
+            };
+            return;
+        }
+        if let Some(functionname) = match_def(&line) {
+            let mut functionpos = self.current_position.clone();
+            functionpos.indentation += 1;
+            self.functions.insert(functionname, functionpos);
+            return;
+        }
+        if line == "RETURN" {
+            match self.stack.pop() {
+                Some(v) => self.current_position = v.clone(),
+                None => panic!("There is nothing on the stack to return to."),
+            }
+            return;
+        }
+        if line == "LOOP" {
+            if self.stack.len() == 0 {
+                return;
+            }
+            match self.stack.get(self.stack.len()-1) {
+                Some(v) => self.current_position = v.clone(),
+                None => (),
+            }
+            return;
+        }
+        if let Some(pos) = self.functions.get(&line) {
+            self.stack.push(self.current_position.clone());
+            self.current_position = pos.clone();
+            return;
+        } else {panic!("Function {:?} not found", line)}
+    }
 }
 
 fn match_assignment(line: &String) -> Option<(String, String)> {
@@ -80,83 +143,6 @@ fn match_def(line: &String) -> Option<String> {
     }
 }
 
-fn run_line(line: &String, state: &mut InterpreterState) {
-    let stripped_line = line.trim_start_matches("\t");
-    let line_indentation = line.len() - stripped_line.len();
-    if line_indentation > state.current_position.indentation {
-        return;
-    }
-    if line_indentation < state.current_position.indentation {
-        state.current_position.indentation = line_indentation;
-    }
-    let line = stripped_line.to_string();
-    match match_assignment(&line) {
-        Some((key, value)) => {
-            let value = sanitize_value(value, &state);
-            if key == "OUTPUT" {
-                print!("{}", value);
-                return;
-            }
-            state.memory.insert(key, value);
-            return;
-        },
-        None => (),
-    }
-    match match_if(&line) {
-        Some((key, value)) => {
-            let value = sanitize_value(value, &state);
-            match state.memory.get(&key) {
-                Some(mem_value) => {
-                    if value == *mem_value { // Is dereferencing like this safe? I sure hope it is...
-                        state.current_position.indentation += 1;
-                    }
-                }
-                None => panic!("Variable {:?} is not defined", key),
-            };
-            return;
-        },
-        None => (),
-    }
-    match match_def(&line) {
-        Some(functionname) => {
-            let mut functionpos = state.current_position.clone();
-            functionpos.indentation += 1;
-            state.functions.insert(functionname, functionpos);
-            return;
-        },
-        None => (),
-    }
-    if line == "RETURN" {
-        match state.stack.pop() {
-            Some(v) => state.current_position = v.clone(),
-            None => panic!("There is nothing on the stack to return to."),
-        }
-        return
-    }
-    if line == "LOOP" {
-        if state.stack.len() == 0 {
-            return
-        }
-        match state.stack.get(state.stack.len()-1) {
-            Some(v) => state.current_position = v.clone(),
-            None => (),
-        }
-        return
-    }
-    // if line == "debug" {
-    //     println!("{} - {}: {} {} {} {} {} {} {}", state.current_position.linenumber, state.memory["cell"], state.memory["cell0"], state.memory["cell1"], state.memory["cell2"], state.memory["cell3"], state.memory["cell4"], state.memory["cell5"], state.memory["cell6"]);
-    //     return
-    // }
-    match state.functions.get(&line) {
-        Some(pos) => {
-            state.stack.push(state.current_position.clone());
-            state.current_position = pos.clone();
-            return // Not really needed, but good for consistency
-        },
-        None => panic!("Function {:?} not found", line)
-    }
-}
-
 fn main() {
     let script = read_file();
     let mut scriptlines = Vec::<String>::new(); 
@@ -173,9 +159,10 @@ fn main() {
     };
 
     'mainloop: loop {
-        match scriptlines.get(state.current_position.linenumber) {
-            Some(line) => run_line(&line, &mut state),
-            None => break 'mainloop,
+        if let Some(line) = scriptlines.get(state.current_position.linenumber) {
+            state.run_line(&line);
+        } else {
+            break 'mainloop;
         }
         state.current_position.linenumber += 1;
     }
